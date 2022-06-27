4a32415140dbd62a3579e9762527ac93580dbebe
CRYS <mariano.christopher123@gmail.com>
2022-06-27 17:31:39 +0800

    Added unit test script draft

--- deploy/01-deploy-basic-nft.js
+++ deploy/01-deploy-basic-nft.js
@@ -1,4 +1,6 @@
 const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
@@ -6,10 +8,14 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
     log("------------------------")
     const args = []
-    const basicNft = await deploy("basicNft", {
+    const basicNft = await deploy("BasicNft", {
         from: deployer,
         args: args,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(basicNft.address, args)
+    }
 }
