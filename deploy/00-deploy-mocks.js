77cf36bed5ceb6ff44d77bf08ed2bdedd5c2c9fe
CRYS <mariano.christopher123@gmail.com>
2022-06-28 15:35:26 +0800

    Edit deploy scripts and add utils

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,10 +1,5 @@
-const { network, ethers } = require("hardhat")
-const {
-    DECIMALS,
-    INITIAL_PRICE,
-    developmentChains,
-    networkConfig,
-} = require("../helper-hardhat-config")
+const { network } = require("hardhat")
+const { DECIMALS, INITIAL_PRICE } = require("../helper-hardhat-config")
 
 const BASE_FEE = "250000000000000000" // 0.25 is this the premium in LINK?
 const GAS_PRICE_LINK = 1e9 // link per gas, is this the gas lane? // 0.000000001 LINK per gas
@@ -13,26 +8,27 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
-    let vrfCoordinatorV2Address, subscriptionId
     // If we are on a local development network, we need to deploy mocks!
-    if (developmentChains.includes(network.name)) {
-        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorv2Mock")
-        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
-        const tx = await vrfCoordinatorV2Mock.createSubscription()
-        const txReceipt = await tx.wait(1)
-        subscriptionId = txReceipt.events[0].args.subId
-    } else {
-        vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
-        subscriptionId = networkConfig[chainId].subscriptionId
+    if (chainId == 31337) {
+        log("Local network detected! Deploying mocks...")
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            log: true,
+            args: [BASE_FEE, GAS_PRICE_LINK],
+        })
+        await deploy("MockV3Aggregator", {
+            from: deployer,
+            log: true,
+            args: [DECIMALS, INITIAL_PRICE],
+        })
+
+        log("Mocks Deployed!")
+        log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        log("You are deploying to a local network, you'll need a local network running to interact")
+        log(
+            "Please run `yarn hardhat console --network localhost` to interact with the deployed smart contracts!"
+        )
+        log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     }
-    log("----------------------------")
-    const args = [
-        vrfCoordinatorV2Address,
-        subscriptionId,
-        networkConfig[chainId].gasLane,
-        networkConfig[chainId].mintFee,
-        //tokenURI
-        networkConfig[chainId].callbackGasLimit,
-    ]
 }
 module.exports.tags = ["all", "mocks", "main"]
--- deploy/02-deploy-random-ipfs.js
+++ deploy/02-deploy-random-ipfs.js
@@ -5,4 +5,44 @@ const { verify } = require("../utils/verify")
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+    let tokenUris
+
+    //get the IPFS hashes ofour images(Methods below)
+    //1. With our IPFS node. https://docs.ipfs.io/
+    //2. pinata https://www.pinata.cloud/   pinata-node-sdk
+    //3. nft.storage(uses filecoin network) https://nft.storage/
+    if (process.env.UPLOAD_TO_PINATA == "true") {
+        tokenUris = await handleTokenUris()
+    }
+
+    let vrfCoordinatorV2Address, subscriptionId
+
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorv2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const tx = await vrfCoordinatorV2Mock.createSubscription()
+        const txReceipt = await tx.wait(1)
+        subscriptionId = txReceipt.events[0].args.subId
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
+        subscriptionId = networkConfig[chainId].subscriptionId
+    }
+    log("----------------------------")
+    const args = [
+        vrfCoordinatorV2Address,
+        subscriptionId,
+        networkConfig[chainId].gasLane,
+        networkConfig[chainId].mintFee,
+        //tokenURI
+        networkConfig[chainId].callbackGasLimit,
+    ]
+}
+
+handleTokenUris = async () => {
+    tokenUris = []
+    //store the image in IPFS
+    //store the metadata in IPFS
+
+    return tokenUris
 }
