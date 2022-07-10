56720a42bd601892d24a4e82589e32e5aa18ed41
CRYS <mariano.christopher123@gmail.com>
2022-07-10 21:53:05 +0800

    Initial typescript commit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,13 @@
-require("@nomiclabs/hardhat-waffle")
-require("@nomiclabs/hardhat-etherscan")
-require("hardhat-deploy")
-require("solidity-coverage")
-require("hardhat-gas-reporter")
-require("hardhat-contract-sizer")
-require("dotenv").config()
+import "@typechain/hardhat"
+import "@nomiclabs/hardhat-waffle"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-ethers"
+import "hardhat-deploy"
+import "solidity-coverage"
+import "hardhat-gas-reporter"
+import "hardhat-contract-sizer"
+import "dotenv/config"
+import { HardhatUserConfig } from "hardhat/config"
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
